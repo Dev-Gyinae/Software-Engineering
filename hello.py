905589c1eb49ac03176c0cc2ac61b538368b65b1
Samuel Gyinae <dev.gyinae@gmail.com>
2026-03-10 00:36:17 +0000

    lists

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-''' is this commented out? '''
-print('Hello, world!') 
+name = input("what is your name?")
+print("hello " + name)
+
+name_char = int(len(name))
+
+print (f"your name {name} has {name_char} characters")
