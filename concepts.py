880e7876d71180189d0b16371222dabfe321e2bc
Samuel Gyinae <dev.gyinae@gmail.com>
2026-03-18 15:39:14 +0000

    tuples

---
+++ concepts.py
@@ -0,0 +1,20 @@
+# list = ['big', 'bad', 'wolf']
+# print(list)
+
+# list += ['kaze']
+# print(list)
+
+# print(list[1:-1])
+
+# list.append('kazy')
+# print(list)
+
+# list.pop(3)
+# print(list)
+
+colors = ('red','yellow','green')
+
+static = ('red','blue', 'white')
+tuple(static)
+print(type(colors))
+print(type(static))
